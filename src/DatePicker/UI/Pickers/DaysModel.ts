1a7ea5faa7fb6089a1a066a1f16560388c92c9f8
Dante Herzlos <streat13@gmail.com>
2024-02-07 00:26:58 +0300

    update: decompose DaysModel

--- src/DatePicker/UI/Pickers/DaysModel.ts
+++ src/DatePicker/UI/Pickers/DaysModel.ts
@@ -3,23 +3,30 @@ import { PickerStyleTypesEnum } from "./const/pickerStyleMap";
 import { RangeDate } from "../../helpers/RangeDate";
 import { MultiDate } from "../../helpers/MultiDate";
 
-export class DaysModel {
-  days: { value: number; styleType: PickerStyleTypesEnum }[];
+type CheckDateFnType = (day: number) => false | PickerStyleTypesEnum;
+
+class BaseDaysModel<SelectedDateType> {
+  checkDateFns: CheckDateFnType[] = [
+    (day: number) => this.isNow(day) && PickerStyleTypesEnum.CURRENT,
+    (day: number) => this.isDisabled(day) && PickerStyleTypesEnum.DISABLED,
+  ];
+  days: { value: number; styleType: PickerStyleTypesEnum }[] = [];
   colSpan: number;
   currentDate: Date;
-  selectedDate: RangeDate | Date | MultiDate;
+  selectedDate: SelectedDateType;
   disabledDates: [Date, Date][];
   constructor(
     currentDate: Date,
-    selectedDate: RangeDate | Date,
+    selectedDate: SelectedDateType,
     disabledDates: [Date, Date][],
+    extendCheckDateFns?: CheckDateFnType[],
   ) {
+    extendCheckDateFns && this.checkDateFns.push(...extendCheckDateFns);
+    const firstDayOfMonth = DateUtils.getFirstDayOfMonth(currentDate);
+    this.colSpan = firstDayOfMonth.getDay() - 1;
     this.disabledDates = disabledDates;
     this.currentDate = currentDate;
     this.selectedDate = selectedDate;
-    this.days = this.generateDays();
-    const firstDayOfMonth = DateUtils.getFirstDayOfMonth(currentDate);
-    this.colSpan = firstDayOfMonth.getDay() - 1;
   }
 
   isNow(day: number) {
@@ -28,18 +35,6 @@ export class DaysModel {
       this.currentDate.getFullYear() === now.getFullYear() &&
       this.currentDate.getMonth() === now.getMonth() &&
       day === now.getDate()
-    );
-  }
-
-  isSelected(day: number) {
-    if (!(this.selectedDate instanceof Date)) {
-      return false;
-    }
-
-    return (
-      this.currentDate.getFullYear() === this.selectedDate.getFullYear() &&
-      this.currentDate.getMonth() === this.selectedDate.getMonth() &&
-      day === this.selectedDate.getDate()
     );
   }
 
@@ -59,8 +54,62 @@ export class DaysModel {
     return false;
   }
 
+  protected generateDays() {
+    const firstDayOfMonth = DateUtils.getFirstDayOfMonth(this.currentDate);
+    const lastDayOfMonth = DateUtils.getLastDayOfMonth(this.currentDate);
+    const start = firstDayOfMonth.getDate();
+    const end = lastDayOfMonth.getDate();
+    const days = [];
+    for (let day = start; day <= end; day++) {
+      let styleType: PickerStyleTypesEnum = PickerStyleTypesEnum.DEFAULT;
+      for (const checkDateFn of this.checkDateFns) {
+        const type = checkDateFn(day);
+        if (type) styleType = type;
+      }
+      days.push({ value: day, styleType });
+    }
+    return days;
+  }
+}
+
+export class DaysModel extends BaseDaysModel<Date> {
+  constructor(
+    currentDate: Date,
+    selectedDate: Date,
+    disabledDates: [Date, Date][],
+  ) {
+    const extendCheckDateFns = [
+      (day: number) => this.isSelected(day) && PickerStyleTypesEnum.ACTIVE,
+    ];
+    super(currentDate, selectedDate, disabledDates, extendCheckDateFns);
+    this.days = this.generateDays();
+  }
+  isSelected(day: number) {
+    return (
+      this.currentDate.getFullYear() === this.selectedDate.getFullYear() &&
+      this.currentDate.getMonth() === this.selectedDate.getMonth() &&
+      day === this.selectedDate.getDate()
+    );
+  }
+}
+
+export class RangeDaysModel extends BaseDaysModel<RangeDate> {
+  constructor(
+    currentDate: Date,
+    selectedDate: RangeDate,
+    disabledDates: [Date, Date][],
+  ) {
+    const extendCheckDateFns = [
+      (day: number) =>
+        this.isStartRange(day) && PickerStyleTypesEnum.START_RANGE,
+      (day: number) =>
+        this.isMiddleRange(day) && PickerStyleTypesEnum.MIDDLE_RANGE,
+      (day: number) => this.isEndRange(day) && PickerStyleTypesEnum.END_RANGE,
+    ];
+    super(currentDate, selectedDate, disabledDates, extendCheckDateFns);
+    this.days = this.generateDays();
+  }
   isStartRange(day: number) {
-    if (!(this.selectedDate instanceof RangeDate)) return false;
     const startDate = this.selectedDate.getStartDate();
     return (
       this.currentDate.getFullYear() === startDate.getFullYear() &&
@@ -70,7 +119,6 @@ export class DaysModel {
   }
 
   isMiddleRange(day: number) {
-    if (!(this.selectedDate instanceof RangeDate)) return false;
     const startDate = this.selectedDate.getStartDate();
     const endDate = this.selectedDate.getEndDate();
     const currentDateWithDay = new Date(
@@ -81,10 +129,10 @@ export class DaysModel {
     if (startDate < currentDateWithDay && endDate > currentDateWithDay) {
       return true;
     }
+    return false;
   }
 
   isEndRange(day: number) {
-    if (!(this.selectedDate instanceof RangeDate)) return false;
     const endDate = this.selectedDate.getEndDate();
     return (
       this.currentDate.getFullYear() === endDate.getFullYear() &&
@@ -92,35 +140,30 @@ export class DaysModel {
       day === endDate.getDate()
     );
   }
+}
 
-  private generateDays() {
-    const firstDayOfMonth = DateUtils.getFirstDayOfMonth(this.currentDate);
-    const lastDayOfMonth = DateUtils.getLastDayOfMonth(this.currentDate);
-    const start = firstDayOfMonth.getDate();
-    const end = lastDayOfMonth.getDate();
-    const days = [];
-    for (let day = start; day <= end; day++) {
-      let styleType: PickerStyleTypesEnum = PickerStyleTypesEnum.DEFAULT;
-      if (this.isNow(day)) {
-        styleType = PickerStyleTypesEnum.CURRENT;
+export class MultiDaysModel extends BaseDaysModel<MultiDate> {
+  constructor(
+    currentDate: Date,
+    selectedDate: MultiDate,
+    disabledDates: [Date, Date][],
+  ) {
+    const extendCheckDateFns = [
+      (day: number) => this.isSelected(day) && PickerStyleTypesEnum.ACTIVE,
+    ];
+    super(currentDate, selectedDate, disabledDates, extendCheckDateFns);
+    this.days = this.generateDays();
+  }
+  isSelected(day: number) {
+    for (const date of this.selectedDate.getValues()) {
+      if (
+        this.currentDate.getFullYear() === date.getFullYear() &&
+        this.currentDate.getMonth() === date.getMonth() &&
+        day === date.getDate()
+      ) {
+        return true;
       }
-      if (this.isDisabled(day)) {
-        styleType = PickerStyleTypesEnum.DISABLED;
-      }
-      if (this.isSelected(day)) {
-        styleType = PickerStyleTypesEnum.ACTIVE;
-      }
-      if (this.isStartRange(day)) {
-        styleType = PickerStyleTypesEnum.START_RANGE;
-      }
-      if (this.isMiddleRange(day)) {
-        styleType = PickerStyleTypesEnum.MIDDLE_RANGE;
-      }
-      if (this.isEndRange(day)) {
-        styleType = PickerStyleTypesEnum.END_RANGE;
-      }
-      days.push({ value: day, styleType });
     }
-    return days;
+    return false;
   }
 }
